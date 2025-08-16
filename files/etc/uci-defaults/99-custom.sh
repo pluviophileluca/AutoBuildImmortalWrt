1e687a9f92bbbdb05ea3e7906e03c26233854bdf
LUCA <pluviophileluca@gmail.com>
2025-08-16 09:20:47 +0800

    Update 99-custom.sh

--- files/etc/uci-defaults/99-custom.sh
+++ files/etc/uci-defaults/99-custom.sh
@@ -84,8 +84,8 @@ elif [ "$count" -gt 1 ]; then
         uci set network.lan.ipaddr=$CUSTOM_IP
         echo "custom router ip is $CUSTOM_IP" >> $LOGFILE
     else
-        uci set network.lan.ipaddr='10.0.0.2'
-        echo "default router ip is 10.0.0.2" >> $LOGFILE
+        uci set network.lan.ipaddr='192.168.100.1'
+        echo "default router ip is 192.168.100.1" >> $LOGFILE
     fi
 
 
